8be06fab319904d75f600dafa7fa0714d038d7fd
John Walker <ss.lbw.ss@gmail.com>
2022-06-08 20:16:39 +0800

    chores:nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import { readerClient } from './db'
+import 'dotenv/config'
 
 const app = express()
 const port = '4396'
 
-app.get('/', (_, res) => {
-  res.send('Hello World!')
+app.get('/', async (_, res) => {
+  const result = await readerClient.from('test').select('test')
+  console.log(result)
+  res.send(JSON.stringify(result))
 })
 
 app.listen(port, () => {
